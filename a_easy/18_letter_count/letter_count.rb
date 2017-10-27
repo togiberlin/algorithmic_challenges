65aae78657a9df99b818f67adab5b560ad9222c6
Togi Dashnyam <togi.berlin@gmail.com>
2017-10-27 21:40:42 +0200

    Dudu dum dum dum, another one bites the dust

---
+++ a_easy/18_letter_count/letter_count.rb
@@ -0,0 +1,19 @@
+def LetterCountI(str)
+    words = str.split(" ")
+    hash_map = Hash.new(0)
+
+    words.each do
+      |word| hash_map[word] = count_repeated(word)
+    end
+
+    hash_map.values.max > 0 ? hash_map.key(hash_map.values.max) : -1
+end
+
+def count_repeated(word)
+    hash_map = Hash.new(0)
+    word.chars { |letter| hash_map[letter] += 1 }
+    hash_map.count { |x, y| y > 1 }
+end
+
+# keep this function call here
+puts ArrayAdditionI(STDIN.gets)
