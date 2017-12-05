40e71b2c568cbe3d96f12b964fdda525de0f60e5
Togi Dashnyam <togi.berlin@gmail.com>
2017-12-05 23:15:29 +0100

    Solved LetterCapitalize

---
+++ a_easy/06_letter_capitalize/letter_capitalize.js
@@ -0,0 +1,7 @@
+function letterCapitalize(str) {
+  var array = str.split(" ");
+
+  return array.map(function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1, string.length);
+  }).join(" ")
+}
