86cf12b80c3d8f4603ea1dff8f0b3a04b660c047
Togi Dashnyam <togi.berlin@gmail.com>
2017-12-06 13:13:27 +0100

    Refactored ab_check.rb

--- a_easy/11_ab_check/ab_check.rb
+++ a_easy/11_ab_check/ab_check.rb
@@ -3,17 +3,19 @@ def ab_check(str)
 
   for n in 0..str.length-1 do
     if str[n] == 'a'
-      if str[n+4] == 'b'
-        bools << 'true'
+      if str[n + 4] == 'b'
+        bools << true
       else
-        bools << 'false'
+        bools << false
+      end
+    elsif str[n] == 'b'
+      if str[n + 4] == 'a'
+        bools << true
+      else
+        bools << false
       end
     end
   end
 
-  if bools.include? 'true'
-    true
-  else
-    false
-  end
+  bools.include? true
 end
