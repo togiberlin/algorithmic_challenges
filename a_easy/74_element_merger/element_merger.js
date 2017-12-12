45bc6fbe33bc6e80821a099d809a22b35f00c8b3
Togi Dashnyam <togi.berlin@gmail.com>
2017-12-12 16:40:54 +0100

    Solved ElementMerger

---
+++ a_easy/74_element_merger/element_merger.js
@@ -0,0 +1,13 @@
+function elementMerger(arr) {
+  // Base case
+  if (arr.length === 1) { return arr[0]; }
+
+  // Recursive case
+  for (i = 0; i < arr.length; i++) {
+    arr[i] = Math.abs(arr[i + 1] - arr[i]);
+  }
+  
+  arr.pop();
+
+  return elementMerger(arr);
+}
