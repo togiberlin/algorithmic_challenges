24681d840a00fd7cf4cbe94b234072e9c43f14b0
Togi Dashnyam <togi.berlin@gmail.com>
2017-12-12 16:21:59 +0100

    Solved SumMultiplier

---
+++ a_easy/71_sum_multiplier/sum_multiplier.js
@@ -0,0 +1,35 @@
+function sumMultiplier(arr) {
+  let combinations = [];
+  let minimumThreshold = arr.reduce((a, b) => a + b) * 2;
+
+  // Remove duplicates
+  arr = arr.filter((elem, index, self) => index === self.indexOf(elem));
+
+  // Make repeated combinations
+  for (i = 0; i < arr.length; i++) {
+    for (n = 0; n < arr.length; n++) {
+      combinations.push([arr[i], arr[n]]);
+    }
+  }
+
+  // Multiply all combinations with each other
+  combinations = combinations.map(([a, b]) => a * b);
+
+  // Remove duplicates
+  combinations = combinations.filter((elem, index, self) => index === self.indexOf(elem));
+
+  // Determine, whether at least 2 values are above threshold
+  function hasTwoValsOverThreshold(productArray) {
+    let counter = 0;
+
+    for (i = 0; i < productArray.length; i++) {
+      if (productArray[i] > minimumThreshold) {
+        counter++;
+      }
+    }
+
+    return counter >= 2 ? true : false;
+  }
+
+  return hasTwoValsOverThreshold(combinations);
+}
