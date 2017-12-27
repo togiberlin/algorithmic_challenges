2d6b5c17b3b99aedf0a99a52be7d4536a3ada97b
Togi Dashnyam <togi.berlin@gmail.com>
2017-12-27 12:35:39 +0100

    Solved Hamming Distance

---
+++ a_easy/34_hamming_distance/hamming_distance.js
@@ -0,0 +1,14 @@
+function hammingDistance(strArr) {
+  strArr = strArr.map(elem => elem.split(""));
+  let counter = 0;
+
+  for (i = 0; i < 1; i++) {
+    for (n = 0; n < strArr[i].length; n++) {
+      if (strArr[i][n] !== strArr[i + 1][n]) {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
+}
