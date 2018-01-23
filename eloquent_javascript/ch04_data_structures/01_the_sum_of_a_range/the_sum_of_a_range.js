77c2092931ea55fc97713aa1e8762a6514caa778
Togi Dashnyam <togi.berlin@gmail.com>
2018-01-23 17:19:20 +0100

    Solved EJS Sum of a Range

---
+++ eloquent_javascript/ch04_data_structures/01_the_sum_of_a_range/the_sum_of_a_range.js
@@ -0,0 +1,24 @@
+function range(start, end, step = 1) {
+  let array = [];
+
+  if (step >= 0) {
+    for (i = start; i <= end; i += step) {
+      array.push(i);
+    }
+  } else if (step < 0) {
+    for (i = start; i >= end; i += step) {
+      array.push(i);
+    }
+  }
+
+  return array;
+}
+
+function sum(array) {
+  return array.reduce((a, b) => a + b);
+}
+
+range(1, 10); // creates array of 1 to 10
+sum(range(1, 10)); // returns 55
+range(1, 10, 2); // [1, 3, 5, 7, 9]
+range(5, 2, -1); // [5, 4, 3, 2]
