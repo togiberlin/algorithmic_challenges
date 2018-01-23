4f990d65d5ca54b190964c624b130e5c84e0694e
Togi Dashnyam <togi.berlin@gmail.com>
2018-01-23 17:47:40 +0100

    Solved EJS ch4 Reversing an Array

---
+++ eloquent_javascript/ch04_data_structures/02_reversing_an_array/reversing_an_array.js
@@ -0,0 +1,27 @@
+function reverseArray(array) {
+  let result = [];
+
+  for (i = array.length - 1; i >= 0; i--) {
+    result.push(array[i]);
+  }
+
+  return result;
+}
+
+test1 = [1, 2, 4, 6, 7];
+console.log("Original array: [" + test1 + "]");
+console.log("Reversed array: [" + reverseArray(test1) + "]");
+
+function reverseArrayInPlace(array) {
+  let swapArray = array.slice(); // create a copy of array
+
+  for (i = 0; i < array.length; i ++) {
+    array[i] = swapArray[array.length - 1 - i];
+  }
+
+  return array;
+}
+
+test2 = [1, 7, 8, 10, 12];
+console.log("Original array: [" + test2 + "]");
+console.log("Reversed array: [" + reverseArrayInPlace(test2) + "]");
